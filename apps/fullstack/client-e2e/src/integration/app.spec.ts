3143aa84b707339abdb1b140961038f1c77c925e
yurikrupnik <krupnik.yuri@gmail.com>
2021-09-08 03:43:03 +0300

    init

--- apps/fullstack/client-e2e/src/integration/app.spec.ts
+++ apps/fullstack/client-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('fullstack-client', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to fullstack-client!');
+    // getGreeting().contains('Welcome to fullstack-client!');
   });
 });
---
+++ apps/fullstack/users-api/src/main.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to users-api!' });
+});
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
