2e6a6c465f4b303f31177e5e5bb7bd5850daff7b
yurikrupnik <krupnik.yuri@gmail.com>
2021-09-08 14:08:40 +0300

    added usesrs docker

---
+++ Makefile
@@ -0,0 +1,2 @@
+ra:
+	npx nx run-many --target=serve --all --parallel --maxParallel=6
--- apps/fullstack/users-api/src/main.ts
+++ apps/fullstack/users-api/src/main.ts
@@ -6,6 +6,8 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to users-api!' });
 });
 
+console.log('process.env.port', process.env.port);
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
