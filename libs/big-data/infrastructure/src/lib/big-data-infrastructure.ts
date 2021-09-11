c7c488ae60787ee129905cb61e93a447cfa0cfba
yurikrupnik <krupnik.yuri@gmail.com>
2021-09-11 23:38:54 +0300

    testing pulumim ci

--- libs/big-data/infrastructure/src/lib/big-data-infrastructure.ts
+++ libs/big-data/infrastructure/src/lib/big-data-infrastructure.ts
@@ -1,3 +1,8 @@
-export function bigDataInfrastructure(): string {
-  return 'big-data-infrastructure';
+import * as gcp from '@pulumi/gcp';
+
+export function bigDataInfrastructure(location: string) {
+  return new gcp.storage.Bucket('temp-folder', {
+    location,
+    forceDestroy: true,
+  });
 }
