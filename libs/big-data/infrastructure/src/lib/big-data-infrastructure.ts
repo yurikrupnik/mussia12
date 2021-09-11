1bb114621ba50e47bee0cf561aeb79479ec63c13
yurikrupnik <krupnik.yuri@gmail.com>
2021-09-11 23:48:50 +0300

    ficed test

--- libs/big-data/infrastructure/src/lib/big-data-infrastructure.ts
+++ libs/big-data/infrastructure/src/lib/big-data-infrastructure.ts
@@ -1,8 +1,12 @@
 import * as gcp from '@pulumi/gcp';
 
-export function bigDataInfrastructure(location: string) {
+export function creteSomething(location: string) {
   return new gcp.storage.Bucket('temp-folder', {
     location,
     forceDestroy: true,
   });
+}
+
+export function bigDataInfrastructure(): string {
+  return 'big-data-infrastructure';
 }
