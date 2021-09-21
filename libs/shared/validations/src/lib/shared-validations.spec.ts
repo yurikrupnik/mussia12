5ec4f1067ebf41233d7ec3f4ea92edff286f5763
yurikrupnik <krupnik.yuri@gmail.com>
2021-09-21 12:03:20 +0300

    added module files

---
+++ libs/shared/validations/src/lib/shared-validations.spec.ts
@@ -0,0 +1,7 @@
+import { sharedValidations } from './shared-validations';
+
+describe('sharedValidations', () => {
+  it('should work', () => {
+    expect(sharedValidations()).toEqual('shared-validations');
+  });
+});
