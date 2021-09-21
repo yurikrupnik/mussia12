527270aa6ba167bd4b46e5a86d44582a65a83c84
yurikrupnik <krupnik.yuri@gmail.com>
2021-09-22 00:26:00 +0300

    updated modules

--- scripts/bash-test.sh
+++ scripts/bash-test.sh
@@ -6,6 +6,11 @@ set -e
 echo Branch name: ${GITHUB_REF##*/}
 echo HEAD_REF: $HEAD_REF
 echo sha: ${GITHUB_SHA::8}
+echo BRA: ${BRA}
+echo BRA: $BRA
 
 
-
+if [[ $HEAD_REF == *"/"* ]]; then
+    HEAD_REF=${GITHUB_SHA::8}
+    echo "It's there!"
+  fi
