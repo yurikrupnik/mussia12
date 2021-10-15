a5fc3936f92c4187d99c4577d57358e22bbb764c
yurikrupnik <krupnik.yuri@gmail.com>
2021-10-15 22:33:28 +0300

    removed users-api express

--- apps/fullstack/users-api/src/app/api/users/model.ts
+++
@@ -1,6 +0,0 @@
-import { UserSchema, UserDocument } from '@mussia12/shared/mongoose-schemas';
-import mongoose from 'mongoose';
-
-export default mongoose.model('users', UserSchema);
-
-export { UserDocument };
