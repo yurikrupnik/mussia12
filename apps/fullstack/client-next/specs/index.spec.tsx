717b9e0833409b5d9a60ec98041649d2fc93b61a
yurikrupnik <krupnik.yuri@gmail.com>
2021-10-22 00:42:45 +0300

    fixed test

--- apps/fullstack/client-next/specs/index.spec.tsx
+++ apps/fullstack/client-next/specs/index.spec.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Index from '../pages/index';
+
+const queryClient = new QueryClient();
 
 describe('Index', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Index />);
+    const { baseElement } = render(
+      <QueryClientProvider client={queryClient}>
+        <Index />
+      </QueryClientProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
