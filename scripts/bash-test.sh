fb9449f106e7dc977d0b2fe54e7624503df1bf35
yurikrupnik <krupnik.yuri@gmail.com>
2021-10-23 21:40:33 +0300

    fixed test

--- scripts/bash-test.sh
+++ scripts/bash-test.sh
@@ -2,17 +2,16 @@
 
 set -e
 
-ds=$(gcloud secrets versions access latest --secret="MONGO_URI")
-echo $ds
-#echo Branch name: ${GITHUB_REF##*/}
-#echo HEAD_REF: $HEAD_REF
-#echo sha: ${GITHUB_SHA::8}
-#echo BRA: ${BRA}
-#echo BRA: $BRA
-#
-#
-#if [[ $BRA == *"/"* ]]; then
-#    HEAD_REF=${GITHUB_SHA::8}
-#    echo "It's there!"
-#    echo $HEAD_REF
-#  fi
+
+echo Branch name: ${GITHUB_REF##*/}
+echo HEAD_REF: $HEAD_REF
+echo sha: ${GITHUB_SHA::8}
+echo BRA: ${BRA}
+echo BRA: $BRA
+
+
+if [[ $BRA == *"/"* ]]; then
+    HEAD_REF=${GITHUB_SHA::8}
+    echo "It's there!"
+    echo $HEAD_REF
+  fi
